4c391f1210509094aa95817e906242d9ab87ba7f
Tajdang <136824908+Dangkeys@users.noreply.github.com>
2024-07-15 12:00:39 +0700

    chore: Refactor yinYang.js to use an array of user inputs

---
+++ loop and function/problems/yinYang.js
@@ -0,0 +1,13 @@
+//เขียนภาษา JS เพื่อวาดรูปหยิน-หยาง ซึ่งจะรับ input เป็นขนาดของรูปหยิน-หยาง และวาดรูปตามขนาดที่กำหนด 
+
+let userInputs = [
+    { name: 'Wacky Waves', size: 1 },
+    { name: 'Silly Swirls', size: 2 },
+    { name: 'Goofy Grid', size: 3 },
+    { name: 'Crazy Circles', size: 4 },
+    { name: 'Bizarre Blocks', size: 5 },
+];
+
+
+
+
--- loop and function/solution/yinYang.js
+++ loop and function/solution/yinYang.js
@@ -1,29 +1,39 @@
-let userInput = 5;
+let userInputs = [
+    { name: 'Wacky Waves', size: 1 },
+    { name: 'Silly Swirls', size: 2 },
+    { name: 'Goofy Grid', size: 3 },
+    { name: 'Crazy Circles', size: 4 },
+    { name: 'Bizarre Blocks', size: 5 },
+];
 
-function getSize() {
+function getSize(userInput) {
     return (2 * userInput) + 4;
 }
 
-function getHalfSize() {
-    return getSize() / 2;
+function getHalfSize(userInput) {
+    return getSize(userInput) / 2;
 }
 
-for (let row = 0; row < getSize(); row++) {
-    let line = '';
-    for (let col = 0; col < getSize(); col++) {
-        if (row < getHalfSize()) {
-            if (col >= getHalfSize()) {
-                line += (row >= 1 && row <= userInput && col < getSize() - 1 && col >= getSize() - userInput - 1) ? '#' : '+';
+userInputs.forEach((input) => {
+    console.log(input.name);
+    for (let row = 0; row < getSize(input.size); row++) {
+        let line = '';
+        for (let col = 0; col < getSize(input.size); col++) {
+            if (row < getHalfSize(input.size)) {
+                if (col >= getHalfSize(input.size)) {
+                    line += (row >= 1 && row <= input.size && col < getSize(input.size) - 1 && col >= getSize(input.size) - input.size - 1) ? '#' : '+';
+                } else {
+                    line += (row + col + 1 < getHalfSize(input.size)) ? '.' : '#';
+                }
             } else {
-                line += (row + col + 1 < getHalfSize()) ? '.' : '#';
-            }
-        } else {
-            if (col >= getHalfSize()) {
-                line += (row + col > getSize() + userInput + 1) ? '.' : '+';
-            } else {
-                line += (row > getHalfSize() && row < getSize() - 1 && col >= 1 && col < getHalfSize() - 1) ? '+' : '#';
+                if (col >= getHalfSize(input.size)) {
+                    line += (row + col > getSize(input.size) + input.size + 1) ? '.' : '+';
+                } else {
+                    line += (row > getHalfSize(input.size) && row < getSize(input.size) - 1 && col >= 1 && col < getHalfSize(input.size) - 1) ? '+' : '#';
+                }
             }
         }
+        console.log(line);
     }
-    console.log(line);
-}
+    console.log();
+});
